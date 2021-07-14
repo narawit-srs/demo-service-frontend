c8b02bb4546bdd515fb8745b872b9eae5b108539
narawit rattanasuriya <narawit@sirisoft.co.th>
2021-07-14 18:51:28 +0700

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { environment } from './../environments/environment';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable()
 export class AppComponent {
   title = 'angular-tour-of-heroes';
+  apiEmployee = environment.apiEmployee;
+  apiCompany = environment.apiCompany;
+
 }
