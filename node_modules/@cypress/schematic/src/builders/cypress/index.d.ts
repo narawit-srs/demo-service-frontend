7da42647793f1fa5162bcad3a281afb8ed3aa6d6
narawit rattanasuriya <narawit@sirisoft.co.th>
2021-07-15 12:09:13 +0700

    x

--- node_modules/@cypress/schematic/src/builders/cypress/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Observable } from 'rxjs';
-import { CypressBuilderOptions } from './cypressBuilderOptions';
-export declare function startDevServer(devServerTarget: string, watch: boolean, context: any): Observable<string>;
-declare const _default: import("@angular-devkit/architect/src/internal").Builder<CypressBuilderOptions & import("@angular-devkit/core").JsonObject>;
-export default _default;
