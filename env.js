23acdecee8f804585e926767f4b6765c77bbf8df
Settawat Janpuk <settawat-srs@Settawats-MBP.Sirisoft-GW>
2021-07-15 11:58:02 +0700

    add unit test

---
+++ env.js
@@ -0,0 +1,11 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.apiUrl = "http://dev.your-api.com";
+
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = true;
+  window.__env.title = "My scrum log (Dev)";
+})(this);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,7 +13,7 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-  
+
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -31,6 +31,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tour-of-heroes app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Welcome to my homepage');
   });
 });
